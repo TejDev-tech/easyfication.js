0738b50c5cfd60401e3912349ca3a54119999b3a
Tejas Shrimali <78650496+TejDev-tech@users.noreply.github.com>
2021-06-01 13:40:53 +0530

    Update easy.js

--- easy.js
+++ easy.js
@@ -1,12 +1,4 @@
 var {random,pow,sqrt,cbrt,round,ceil,floor} = Math;
-
-var  functions = {
- add : add  = (a,b) =>  a + b,
- sub  : sub = (a,b) =>  a - b, 
- mult :  mult = (a,b) =>  a * b,
- div :  div = (a,b) =>  a/b ,
-}
-
-var {add,sub,mult,div} = functions;
+var {log} = console
 
 const selectQuery = (a) => document.querySelector(a);
